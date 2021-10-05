aad2650f6e0df76abdc869c257980504e1c4afd8
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-05 20:53:01 +0200

    uses react-icons lib

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { IconContext } from 'react-icons'
+import { FaReact } from 'react-icons/fa'
+import { MdAlarm } from 'react-icons/md'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <IconContext.Provider value={{ size: '15rem', color: 'yellow' }}>
+      <div className='App'>
+        <FaReact />
+        <MdAlarm />
+      </div>
+    </IconContext.Provider>
+  )
 }
 
-export default App;
+export default App
