5752ab3ec9f85242366d12fc1734451c50d24565
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-08 16:03:45 +0200

    uses react-credit-card library

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ import IdleTimerContainer from './components/IdleTimerContainer'
 
 import { ChromePicker } from 'react-color'
 
+import Cards from 'react-credit-cards'
+import 'react-credit-cards/es/styles-compiled.css'
+
 toast.configure()
 
 const ChildrenTooltip = forwardRef((props, ref) => {
@@ -43,6 +46,12 @@ function App() {
   const [modalIsOpen, setModalIsOpen] = useState(false)
   const [showColorPicker, setShowColorPicker] = useState(false)
   const [color, setColor] = useState('#fff')
+
+  const [number, setNumber] = useState('')
+  const [name, setName] = useState('')
+  const [expiry, setExpiry] = useState('')
+  const [cvc, setCvc] = useState('')
+  const [focus, setFocus] = useState('')
 
   const { countUp, start, pauseResume, reset, update } = useCountUp({
     duration: 5,
@@ -122,6 +131,48 @@ function App() {
         />
       )}
       <h2>You picked {color}</h2>
+
+      <Cards
+        number={number}
+        name={name}
+        expiry={expiry}
+        cvc={cvc}
+        focused={focus}
+      />
+      <form>
+        <input
+          type='tel'
+          name='number'
+          placeholder='Card Number'
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type='text'
+          name='name'
+          placeholder='Card Name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type='text'
+          name='expiry'
+          placeholder='MM/YY Expiry'
+          value={expiry}
+          onChange={(e) => setExpiry(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type='tel'
+          name='cvc'
+          placeholder='CVC'
+          value={cvc}
+          onChange={(e) => setCvc(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+      </form>
     </>
   )
 }
